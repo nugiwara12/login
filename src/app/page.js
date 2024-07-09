f771782deae867cdbaa5540a9429f9f9952891b1
christoper sarmiento <jacinto011200@gmail.como>
2024-07-10 06:09:01 +0800

    add the logo and the login page

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,17 @@ export default function Home() {
   return (
     <div className="flex bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
       <div className="flex flex-col justify-center items-center w-full">
-        <div className="flex justify-center mb-6">
-          <Image src="/img/logo.svg" className="h-6" alt="Logo" width={50} height={50} />
-          <p>Employee Portal</p>
-          <div>
-             <p className="text-[11px]">v1.03.1</p>
-         </div>
+        <div className="flex flex-col items-center sm:flex-row sm:items-end justify-center mb-6">
+          <Image src="/img/logo.svg" className="h-6 mb-2 sm:mb-0" alt="Logo" width={50} height={50} />
+          <p className="ml-2 font-bold text-center sm:text-left text-black">
+            Employee Portal <br />
+            <p className="text-[10px] sm:text-right">v1.03.1</p>
+          </p>
         </div>
 
-        <div className="divide-y divide-gray-300/50 w-full">
+
+
+        <div className="w-full">
           <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
             <div className="flex flex-col space-y-6 justify-center items-center w-full max-w-lg mx-auto p-4">
               <div className="flex justify-center items-center w-full">
